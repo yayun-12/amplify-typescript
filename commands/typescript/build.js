24394e92fcc6d902570663b483e2e1968ff57fb3
MURAKAMI Masahiko <fossamagna2@gmail.com>
2019-06-20 14:07:29 +0900

    Add support multienv

--- commands/typescript/build.js
+++ commands/typescript/build.js
@@ -10,7 +10,10 @@ module.exports = {
         const { amplify, print } = context
        
         let projectDetails = await amplify.getProjectDetails();
-        let basePath = path.join(projectDetails.projectConfig.projectPath, 'amplify/backend/function');
+        const projectPath = projectDetails.projectConfig.projectPath 
+            ? projectDetails.projectConfig.projectPath
+            : projectDetails.localEnvInfo.projectPath;
+        let basePath = path.join(projectPath, 'amplify/backend/function');
         let functions = fs.readdirSync(basePath)
         
         for (const func of functions) {
